2545c14b859a60a9c3da68df401b3f46d99d2ce0
aliasadipoor <aliasadipoor33@gmail.com>
2025-10-24 05:40:37 -0700

    update code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:crypto_currency/data/model/crypto.dart';
 import 'package:crypto_currency/pages/coin_list_page.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -24,28 +25,33 @@ class _HomePageState extends State<HomePage> {
       home: Scaffold(
         backgroundColor: Colors.grey[800],
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [Image(image: AssetImage('assets/images/logo.png'))],
-          ),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(image: AssetImage('assets/images/logo.png')),
+            SpinKitWave(
+              color: Colors.white,
+              size: 30.0,
+            ),
+          ],
+        )),
       ),
     );
   }
 
   Future<void> getData() async {
     var response = await Dio().get(
-      'https://rest.coincap.io/v3/assets?apiKey=8b7482dd07ed74bdc69d8e0afb8158589614b948afe28fbfac2132a5f4d4ebd5',
-    );
-    List<Crypto> cryptoList =
-        response.data['data']
-            .map<Crypto>((jsonMapObject) => Crypto.fromMapJson(jsonMapObject))
-            .toList();
+        'https://rest.coincap.io/v3/assets?apiKey=658ec474b1f482e18ab745c9b26c4cb4a9a4f31486679c749c0e65b8d9b1ab25');
+    List<Crypto> cryptoList = response.data['data']
+        .map<Crypto>((jsonMapObject) => Crypto.fromMapJson(jsonMapObject))
+        .toList();
 
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => CoinListPage(cryptoList: cryptoList),
+        builder: (context) => CoinListPage(
+          cryptoList: cryptoList,
+        ),
       ),
     );
   }
